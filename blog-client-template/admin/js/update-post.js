f6fa47dfd337404b94f09a22573bfd5446babe4b
codeUB <umran.bajwa@cmeducation.se>
2022-03-20 18:05:09 +0100

    final

--- blog-client-template/admin/js/update-post.js
+++ blog-client-template/admin/js/update-post.js
@@ -1,5 +1,24 @@
 window.onload = function() {
     var url = document.URL;
-    var id = url.substring(url.indexOf('/')+1);
+    var id = url.substring(url.indexOf('=')+1);
     fetchPost(id)
+}
+
+async function fetchPost(id) {
+    try {
+        const response = await fetch('http://localhost:5000/posts/' +id)
+        const post = await response.json();
+        
+        
+            document.getElementById("title").value = post.title;
+            document.getElementById('author').value = post.author;
+            document.getElementById("content").value = post.content;
+            document.getElementById('tags').value = post.tags;
+
+
+      
+    } catch(error) {
+        console.log(error)
+    }
+
 }
